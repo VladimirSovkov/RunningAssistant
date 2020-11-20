503dbbccf854210bc8035a061fe0baa1226fab12
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:34:20 +0300

    Add TrainingModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/TrainingModel/Training.cs
@@ -0,0 +1,33 @@
+﻿using RunningAssistant.Domain.Toolkit.Domain;
+using System;
+
+namespace RunningAssistant.Domain.TrainingModel
+{
+    public class Training : Entity
+    {
+        public Training(
+            string name
+            , string type
+            , float Calories
+            , string description = "")
+        {
+
+        }
+        
+        public string Name { get; private set; }
+        public string Type { get; private set; }
+        public string Description { get; private set; }
+        public DateTime Duration { get; private set; }
+        public float Calories { get; private set; }
+
+        public void SetDuration(DateTime time)
+        {
+            ///продолжительност должна быть больше чем ничего
+            if (time < new DateTime())
+            {
+                throw new ArgumentException("Incorrect workout duration");
+            }
+            Duration = time;
+        }
+    }
+}
