43d05319ffbd90b5d6ea3b0108d0b285ac2a4b79
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 22:00:44 +0300

    Добави инструменты для работы с бд

---
+++ src/RunningAssistant.Infrastructure/Data/RunningAssistantContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RunningAssistant.Domain.FoodModel;
+using RunningAssistant.Infrastructure.Data.EntityConfiguration;
+
+namespace RunningAssistant.Infrastructure.Data
+{
+    public class RunningAssistantContext : DbContext
+    {
+        public RunningAssistantContext(DbContextOptions<RunningAssistantContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Food> Food { get; set; }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new FoodMap());
+        }
+    }
+}
---
+++ src/RunningAssistant.Infrastructure/Startup/BaseBindings.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+using RunningAssistant.Infrastructure.Data;
+
+namespace RunningAssistant.Infrastructure.Startup
+{
+    public static class BaseBindings
+    {
+        public static IServiceCollection AddBaseServices(this IServiceCollection services)
+        {
+            return services.AddRepositories();
+        }
+    }
+}
