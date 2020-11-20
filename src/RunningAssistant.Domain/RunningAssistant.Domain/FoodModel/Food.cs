32a2d142f0980d1ef8bd9ea5116d6cb7deb4286b
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:34:03 +0300

    Add FoodModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/FoodModel/Food.cs
@@ -0,0 +1,27 @@
+﻿using RunningAssistant.Domain.Toolkit.Domain;
+
+namespace RunningAssistant.Domain.FoodModel
+{
+    public class Food : Entity
+    {
+        public Food(
+            string name
+            , float colorie = 0
+            , float proteins = 0
+            , float fats = 0
+            , float carbohydrates = 0)
+        {
+            Name = name;
+            Calorie = colorie;
+            Proteins = proteins;
+            Fats = fats;
+            Carbohydrates = carbohydrates;
+        }
+
+        public string Name { get; private set; }
+        public float Calorie { get; private  set; }
+        public float Proteins { get; private set; }
+        public float Fats { get; private set; }
+        public float Carbohydrates { get; private set; } 
+    }
+}
