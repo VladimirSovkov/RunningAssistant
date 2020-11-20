13bd9b82d72fc01044ab9b71ef8fe8a4a8db003f
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 13:39:44 +0300

    add UserCoordinateModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/UserCoordinateModel/UserCoordinate.cs
@@ -0,0 +1,26 @@
+﻿using RunningAssistant.Domain.Toolkit.Domain;
+using RunningAssistant.Domain.UserModel;
+using System;
+
+namespace RunningAssistant.Domain.UserCoordinateModel
+{
+    public class UserCoordinate : Entity
+    {
+        public UserCoordinate (
+            User user
+            , float latitude
+            , float longitude
+            )
+        {
+            User = user;
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+
+        public int IdUser { get; private set; }
+        public virtual User User { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public float Latitude { get; private set; }
+        public float Longitude { get; private set; }
+    }
+}
