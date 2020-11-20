511436254967797f70618c0be2dc94943614a6f3
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:54:38 +0300

    AddTraining

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/UserModel/Result.cs
@@ -0,0 +1,28 @@
+﻿using RunningAssistant.Domain.Toolkit.Domain;
+using RunningAssistant.Domain.TrainingModel;
+using System;
+
+namespace RunningAssistant.Domain.UserModel
+{
+    public class Result : Entity
+    {
+        public Result(
+            User user
+            , Training training
+            , float value)
+        {
+            User = user;
+            Training = training;
+            Value = value;
+        }
+
+        public int UserId { get; private set; }
+        public virtual User User { get; private set; }
+        
+        public int TrainingId { get; private set; }
+        public virtual Training Training { get; private set; }
+
+        public DateTime CreationDate { get; private set; }
+        public float Value { get; private set; }
+    }
+}
