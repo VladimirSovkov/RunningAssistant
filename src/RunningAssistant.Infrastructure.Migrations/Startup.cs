4669577ca0000fd3d02d0db103231bb692e54a6b
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:53:27 +0300

    Начал создавать миграцию

---
+++ src/RunningAssistant.Infrastructure.Migrations/Startup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace RunningAssistant.Infrastructure.Migrations
+{
+    class Startup : IStartupЫ
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
