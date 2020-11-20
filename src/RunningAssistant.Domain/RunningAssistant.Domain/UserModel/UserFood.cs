22d2ae135aa16e3340f1fcbc7d415c12f4fb68ef
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:34:35 +0300

    Add UserFoodModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/UserModel/UserFood.cs
@@ -0,0 +1,22 @@
+﻿using RunningAssistant.Domain.FoodModel;
+using RunningAssistant.Domain.Toolkit.Domain;
+
+namespace RunningAssistant.Domain.UserModel
+{
+    public class UserFood : Entity
+    {
+        public UserFood(
+            User user
+            , Food food)
+        {
+            User = user;
+            Food = food;
+        }
+
+        public int IdUser {get; private set;}
+        public virtual User User {get; private set;}
+
+        public int IdFood { get; private set; }
+        public virtual Food Food { get; private set; }
+    }
+}
