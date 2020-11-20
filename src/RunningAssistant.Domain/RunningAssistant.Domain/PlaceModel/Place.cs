3d969a21b5b573c79128fc8617c8bdcdf80abe94
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:54:03 +0300

    Add PlaceModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/PlaceModel/Place.cs
@@ -0,0 +1,26 @@
+﻿using RunningAssistant.Domain.Toolkit.Domain;
+
+namespace RunningAssistant.Domain.PlaceModel
+{
+    public class Place : Entity
+    {
+        public Place(
+            string street
+            , string city)
+        {
+            Street = street;
+            City = city;
+        }
+
+        public string Street { get; private set; }
+        public string City { get; private set; }
+        public float Latitude { get; private set; }
+        public float Longitude { get; private set; }
+
+        public void SetCoordinate (float latitude, float longitude)
+        {
+            Latitude = latitude;
+            Longitude = Longitude;
+        }
+    }
+}
