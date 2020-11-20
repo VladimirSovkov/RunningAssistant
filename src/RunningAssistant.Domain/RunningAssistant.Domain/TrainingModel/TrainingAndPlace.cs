1a6df0385f210ce8d7ad511b9edabb3e8a48b405
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 14:54:22 +0300

    Add TrainingAndPlacesModel

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/TrainingModel/TrainingAndPlace.cs
@@ -0,0 +1,22 @@
+﻿using RunningAssistant.Domain.PlaceModel;
+using RunningAssistant.Domain.Toolkit.Domain;
+
+namespace RunningAssistant.Domain.TrainingModel
+{
+    public class TrainingAndPlace : Entity
+    {
+        public TrainingAndPlace(
+            Place place
+            , Training training)
+        {
+            Place = place;
+            Training = training;
+        }
+
+        public virtual Place Place { get; private set; }
+        public int PlaceId { get; private set; }
+
+        public virtual Training Training { get; private set; }
+        public int TrainingId { get; private set; }
+    }
+}
