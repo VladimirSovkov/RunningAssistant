070a1057a81a63dd32aeb4bb80e2df021f06c3e9
VladimirSovkov <vladimir.sovkov@yandex.ru>
2020-11-20 13:39:18 +0300

    add Toolkit

---
+++ src/RunningAssistant.Domain/RunningAssistant.Domain/Toolkit/Domain/Entity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RunningAssistant.Domain.Toolkit.Domain
+{
+    public abstract class Entity
+    {
+        public virtual int Id { get; set; }
+        public bool IsTransient()
+        {
+            return Id == default(Int32);
+        }
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Entity))
+                return false;
+
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (GetType() != obj.GetType())
+                return false;
+
+            Entity item = (Entity)obj;
+
+            if (item.IsTransient() || IsTransient())
+                return false;
+
+            return item.Id == Id;
+        }
+
+        int? _requestedHashCode;
+
+        public override int GetHashCode()
+        {
+            if (!IsTransient())
+            {
+                if (!_requestedHashCode.HasValue)
+                    _requestedHashCode =
+                        Id.GetHashCode() ^
+                        31; // XOR for random distribution (http://blogs.msdn.com/b/ericlippert/archive/2011/02/28/guidelines-and-rules-for-gethashcode.aspx)
+
+                return _requestedHashCode.Value;
+            }
+
+            return base.GetHashCode();
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            return Equals(left, null) ? Equals(right, null) : left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return GetType().Name + " [Id=" + Id + "]";
+        }
+    }
+}
